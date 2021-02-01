d6a350718f2e1a9b57b019541e895de311378c82
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-01 17:25:45 -0500

    Api collects five bars from within a 500m radius of user's zipcode input

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # color gem? 
 gem 'pry'
 gem 'httparty'
+gem 'awesome_print'
 #any gems you add also need to be required by your environment
--- lib/api.rb
+++ lib/api.rb
@@ -1,13 +1,29 @@
 class Api
-    # @@api_key = #api key here
-    # @@url = #api url here
 
-    def self.get_bars
-        # response = HTTParty.get(@@url)
-        #maybe begin with hardcoded zipcode just to see
-        #pass uri into 
+
+    def self.get_bars(input)
+        auth = "Bearer mP8GTWHecSg35laV_nS_phhhXktWoT9swULWlpYcdoUD_zrZFrV2kADMUzUb4ID5FRVXJprDaiaXNyw_uGsTj6KvuM6yJ7ZNDJ7zmdX24atV-L6iHo8wX_p9wV8YYHYx"
+        bars = "https://api.yelp.com/v3/businesses/search?term=bars&location=#{input}&radius=500&limit=5"
+        five_bars = HTTParty.get(bars, headers: {"Authorization" => auth})["businesses"]
+        binding.pry
     end
 
 
 end
+
+
+#HERE IS HOW YOU GET 5 bars within 500 meters of a zipcode BARS—                                       
+# bars_url = "https://api.yelp.com/v3/businesses/search?term=bars&location=10001&radius=500&limit=5"
+#inside i need to add check["businesses"]
+    #for each business I need an ID and a name and a url and phone number and price <-maybe make a class of bar??? 
+    #then the user needs to chose a business
+        #then I need to go to the url of the bar and scrape it for mentions of happy hour and the paragraph around it
+        #show the user those mentions
+        #do they want to call the bar to confirm this information? // #do they want to check another bar? // #do they want to exit?
+
+#auth = "Bearer mP8GTWHecSg35laV_nS_phhhXktWoT9swULWlpYcdoUD_zrZFrV2kADMUzUb4ID5FRVXJprDaiaXNyw_uGsTj6KvuM6yJ7ZNDJ7zmdX24atV-L6iHo8wX_p9wV8YYHYx"
+#reviews_url = "https://api.yelp.com/v3/businesses/4wwvIyP5m6AEYE5gRG0CAg/reviews"
+
+#check = HTTParty.get(bars_url, headers: {"Authorization" => auth})
+
 
